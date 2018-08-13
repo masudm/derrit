e1d2603abb2bdde8e9aec318a43f58a041a0aa2c
Masud Mohammed <0pixelz0@gmail.com>
2018-08-13 14:53:49 +0100

    initial setup of api

---
+++ api/app.js
@@ -0,0 +1,23 @@
+//dependencies
+const express = require('express'); //the express web server dependency
+const app = express(); //create a web server using express
+const bodyParser = require('body-parser'); //middleware for parsing body requests
+const jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
+
+//config
+//parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+//parse application/json
+app.use(bodyParser.json());
+
+//API routes
+//create an instance for routes
+var apiRoutes = express.Router();
+
+//middleware - all the routes go through here
+
+//the routes within the base route and what each route does
+app.use('/', require('./routes/v1/index'));
+
+app.listen(8000, () => console.log('Derrit server listening on port 3000!'));
+
