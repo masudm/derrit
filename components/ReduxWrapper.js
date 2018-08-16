df4c11db94111c59d6165b4d4f4c92af52734467
Masud Mohammed <0pixelz0@gmail.com>
2018-08-16 22:02:20 +0100

    change the extend

--- components/ReduxWrapper.js
+++ components/ReduxWrapper.js
@@ -1,9 +1,9 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Provider } from 'react-redux'
 import configureStore from '../store/configureStore.js'
 
 const ReduxWrapper = WrapperProp => {
-  return class WrapperClass extends React.Component {
+  return class WrapperClass extends Component {
     render() {
       return (
         <Provider store={configureStore()}>
