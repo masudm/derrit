24b175589e069f11f7de452d264c5c7af7cfdf79
Masud Mohammed <0pixelz0@gmail.com>
2018-08-26 22:15:17 +0100

    change const

--- actions/postActions.js
+++ actions/postActions.js
@@ -4,15 +4,16 @@ export function getPosts() {
   return async dispatch => {
     dispatch({ type: 'posts/POST_REQUEST' })
     try {
-      const database = firebase.database()
-      const postsRef = database.ref('posts/')
-      postsRef.on('value', snapshot => {
-        const data = snapshot.val()
-        const posts = Object.keys(data).map(key => {
-          return [Number(key), data[key]]
+      firebase
+        .database()
+        .ref('posts/')
+        .on('value', snapshot => {
+          const data = snapshot.val()
+          const posts = Object.keys(data).map(key => {
+            return [Number(key), data[key]]
+          })
+          dispatch({ type: 'posts/POST_SUCCESS', payload: posts })
         })
-        dispatch({ type: 'posts/POST_SUCCESS', payload: posts })
-      })
     } catch (error) {
       dispatch({ type: 'posts/POST_ERROR', payload: error.message })
       throw error
